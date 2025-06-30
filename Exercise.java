da9cae791b568415e3f6dcded11867559ddab9f3
Wilasinee674259021 <674259021@webmail.npru.ac.th>
2025-06-30 17:00:41 +0700

    Add files via upload

---
+++ Exercise.java
@@ -0,0 +1,15 @@
+public class Exercise {
+    public static void main (String[]args){
+        //System.out.println("0"+"5");
+        //System.out.println("0"+"5"+"5");
+        //System.out.println(0+"5");
+        //System.out.println(0+5);
+        //System.out.println("0"+"5"+5);
+        //System.out.println("0"+5);
+        //System.out.println("0"+(5+1));
+        //System.out.println("Software \n\n Engineering");
+        //System.out.println("Software \t\t Engineering");
+        //System.out.println("Software \n\t Engineering");
+
+    }
+}
